8eeca84faeffac7f78a1240e193fa1e99eafac04
MobileDev418 <oncemoreforyou@outlook.com>
2020-01-17 21:58:19 +0800

    project init

---
+++ src/Temp.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+
+import { styles, color, windowWidth } from "./styles/theme"
+
+export default class CloseAccount extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+
+        return (
+            <View style={styles.container}>
+                
+            </View>
+        )
+    }
+}
+
