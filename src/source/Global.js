41c031cd8ff67395e5e7f1f9aa0295cf4a629202
MobileDev418 <oncemoreforyou@outlook.com>
2020-01-22 21:07:53 +0800

    global variable define

---
+++ src/source/Global.js
@@ -0,0 +1,13 @@
+
+export class App {
+
+    static navigation = null
+    static slideIndex = -1
+
+    static default_avatar = "https://firebasestorage.googleapis.com/v0/b/onedoor-74875.appspot.com/o/avatar%2Fdefault_avatar.png?alt=media&token=bb3a092c-b6d0-40a5-b912-1c6552e2951e"
+    static GOOGLE_MAPS_APIKEY = 'AIzaSyDQnNu7t79jqzhO7rt7_SyHwWSXfcbj35k';
+
+    static userId = ""
+    static receiverId = ""
+}
+
