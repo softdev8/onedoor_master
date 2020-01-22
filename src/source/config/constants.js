07e109e42d0613d26769be61a8899ed5cbcfa290
MobileDev418 <oncemoreforyou@outlook.com>
2020-01-22 21:03:14 +0800

    define constants

---
+++ src/source/config/constants.js
@@ -0,0 +1,6 @@
+export const FIREBASE_API_KEY = "AIzaSyB8tpFVFehoN-56AZ4qCB6UI8r92dbnhDc";
+export const FIREBASE_AUTH_DOMAIN = "onedoor-74875.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://onedoor-74875.firebaseio.com/";
+export const FIREBASE_PROJECT_ID = "onedoor-74875";
+export const FIREBASE_STORAGE_BUCKET = "onedoor-74875.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "195342534022";
